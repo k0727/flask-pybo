70cd726f9f24f85a67a85752027c8604b4dc3c62
njkim <njkim@paran.com>
2021-01-05 01:10:32 +0900

    Chapter 02-10

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -7,19 +7,20 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
 
-    #ORM
+    # ORM
     db.init_app(app)
     migrate.init_app(app, db)
     from . import models
 
-    #블루프린트
-    from .views import main_views, question_views
+    # 블루프린트
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
-
